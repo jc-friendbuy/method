7442cfb155db0dd117ce27f91fe4052f9ff4ae70
Juan Carlos Coto <jccotou@gmail.com>
2015-10-16 18:00:28 -0600

    Rename imported r2_score function to _r2_score to avoid polluting namespace

--- src/model/goodness_of_fit.py
+++ src/model/goodness_of_fit.py
@@ -1,7 +1,8 @@
 # Author and copyright: Juan Carlos Coto, 2015.  Usage under explicit instruction only.
-# Measurements of goodness of fit.
+# Measurements of goodness of fit.  These functions calculate a value that indicates a measure of
+# distance between vectors.
 from numpy import ndarray
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score as _r2_score
 
 # Calculare the difference of the squares of both vectors.
 def difference_of_squares(x, y):
@@ -11,7 +12,7 @@ def difference_of_squares(x, y):
 # Calculate the coefficient of determination between both vectors (a.k.a. R^2)
 def coefficient_of_determination(x, y):
     _assert_vectors_are_ndarray_and_dimensions_are_compatible(x, y)
-    return r2_score(x, y)
+    return _r2_score(x, y)
 
 # Assert that input arrays are ndarrays and their dimensions are equal (to support subtraction)
 def _assert_vectors_are_ndarray_and_dimensions_are_compatible(x, y):
