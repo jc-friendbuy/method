2a6f87406b880e9c85d5231e1ca7b8f072fcb65c
Juan Carlos Coto <jccotou@gmail.com>
2015-10-16 18:03:11 -0600

    Move all files to method directory to adopt PyPi format

---
+++ method/test/model/test_goodness_of_fit.py
@@ -0,0 +1,9 @@
+
+import unittest
+
+class PrimesTestCase(unittest.TestCase):
+    """Tests for `primes.py`."""
+
+    def test_is_five_prime(self):
+        """Is five successfully determined to be prime?"""
+        self.assertEquals(5, 5)
