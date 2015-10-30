Fix goodness of fit R^2 calculation and add test for it

method/model/goodness_of_fit.py
@@ -1,20 +1,45 @@
 # Author and copyright: Juan Carlos Coto, 2015.  Usage under explicit instruction only.
 # Measurements of goodness of fit.  These functions calculate a value that indicates a measure of
 # distance between vectors.
-from numpy import ndarray
+import numpy as np
 from sklearn.metrics import r2_score as _r2_score
 
-# Calculare the difference of the squares of both vectors.
 def difference_of_squares(x, y):
-    _assert_vectors_are_ndarray_and_dimensions_are_compatible(x, y)
+    """
+    Calculare the difference of the squares of both vectors.
+    """
+    _validate_arrays(x, y)
     return sum((x - y) ^ 2)
 
-# Calculate the coefficient of determination between both vectors (a.k.a. R^2)
-def coefficient_of_determination(x, y):
-    _assert_vectors_are_ndarray_and_dimensions_are_compatible(x, y)
-    return _r2_score(x, y)
+def coefficient_of_determination(observed, predicted):
+    """
+    Calculate the coefficient of determination (a.k.a. R^2) between both arrays (observed and
+    predicted as usually called).
+    """
+    _validate_arrays(observed, predicted)
+    return _r2_score(observed, predicted)s
 
-# Assert that input arrays are ndarrays and their dimensions are equal (to support subtraction)
-def _assert_vectors_are_ndarray_and_dimensions_are_compatible(x, y):
-    assert isinstance(ndarray, x) and isinstance(ndarray, y) and x.shape == y.shape,
-        "Both input vectors must be numpy ndarrays of equal dimension"
+def _validate_arrays(x, y):
+    """
+    Assert that input arrays are ndarrays and their dimensions are equal (to support subtraction)
+    """
+    _assert_array_is_ndarray(x)
+    _assert_array_is_ndarray(y)
+    _assert_array_is_double_precision(x)
+    _assert_array_is_double_precision(y)
+    _assert_array_shapes_are_equivalent(x, y)
+
+def _assert_array_is_ndarray(a):
+    """
+    Assert if an array is of datatype ndarray (numpy n-dimensional array).
+    """
+    assert isinstance(a, np.ndarray), "The input array is not ndarray."
+
+def _assert_array_shapes_are_equivalent(a, b):
+    """
+    Assert that the shapes of both provided arrays are equivalent.
+    """
+    assert a.shape == b.shape, "The shapes of the input arrays are not equivalent."
+
+def _assert_array_is_double_precision(a):
+    assert a.dtype == np.float64, "The input array's data type is different from float64."

test/model/goodness_of_fit/test_goodness_of_fit.py
@@ -1,19 +1,27 @@
 
 import unittest
 import pytest
-from numpy import ndarray
+import numpy as np
 from method.model import goodness_of_fit
+from test.common import assert_values_are_within_epsilon_distance
 
-class GoodnessOfFitTestCase(unittest.TestCase):
-    """Tests for `model/goodness_of_fit.py`."""
+"""Tests for `model/goodness_of_fit.py`."""
 
-    test_data = [
-        (ndarray([1, 2, 3, 4, 5]), ndarray([1, 4, 9, 16, 25]))
-    ]
+test_data = [
+    ([1, 2, 3, 4, 5], [1.9, 3.7, 5.8, 8.0, 9.6])
+]
 
-    @pytest.mark.parametrize("x, y", test_data)
-    def test_r2_equals_known_value(self, x, y):
-        """Is the R^2 calculation for x and y equals to a known quantity that is correct?"""
-        mean_x = x.mean(dtype=np.float64)
-        expected_value = sum((y - m) ^ 2) / sum((x - m) ^ 2)
-        self.assertEquals(goodness_of_fit.coefficient_of_determination(x, y), expected_value)
+@pytest.mark.parametrize("observed,predicted", test_data)
+def test_r2_equals_known_value(observed, predicted):
+    """
+    Is the R^2 calculation for x and predicted equals to a known quantity that is correct?
+    """
+    observed = np.array(observed, np.float64)
+    predicted = np.array(predicted, np.float64)
+    m = np.mean(observed, dtype=np.float64)
+    total_sum_of_squares = np.sum(np.power(observed - m, 2))
+    residual_sum_of_squares = np.sum(np.power(observed - predicted, 2))
+
+    expected_value = 1 - (residual_sum_of_squares / total_sum_of_squares)
+    calculated_value = goodness_of_fit.coefficient_of_determination(observed, predicted)
+    assert_values_are_within_epsilon_distance(calculated_value, expected_value)