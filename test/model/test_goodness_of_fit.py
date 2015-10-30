cfbb089a13765b747d20e866e87468298351921e
Juan Carlos Coto <jccotou@gmail.com>
2015-10-30 09:03:23 -0600

    Fix test.py command line for executing and update import path to include test/ dir

--- test/model/test_goodness_of_fit.py
+++
@@ -1,9 +0,0 @@
-
-import unittest
-
-class PrimesTestCase(unittest.TestCase):
-    """Tests for `primes.py`."""
-
-    def test_is_five_prime(self):
-        """Is five successfully determined to be prime?"""
-        self.assertEquals(5, 5)
