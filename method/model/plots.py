7cd82f5a32d1a07738d825664f3b6309ffd9752c
Juan Carlos Coto <jccotou@gmail.com>
2015-11-01 17:04:10 -0600

    Add empty scatter_fit method

--- method/model/plots.py
+++ method/model/plots.py
@@ -1,2 +1,7 @@
 # Author and copyright: Juan Carlos Coto, 2015.  Usage under explicit instruction only.
 # Plotting code will be here once the migration from R is complete.
+
+import matplotlib.pyplot as plt
+
+def scatter_fit(training_set, validation_set, model_results):
+    pass
