077b4c12f3001550d14aa5239a83971a46e7a7a9
Juan Carlos Coto <jccotou@gmail.com>
2015-11-03 18:16:30 -0600

    Remove old, unnecessary tests

--- test/simple_read.py
+++
@@ -1,8 +0,0 @@
-# Author and copyright: Juan Carlos Coto, 2015.  Usage under explicit instruction only.
-
-import sys
-sys.path.append("../")
-
-from data import ccle
-
-print ccle.get_gene_profile_view().size
