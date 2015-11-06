ea79e4403ceba3440721fef8a1586a0a806b9e80
Juan Carlos Coto <jccotou@gmail.com>
2015-11-05 18:16:54 -0600

    Add line and No (i.e. nothing) trend formatters

---
+++ method/helpers/plotting/trend_formatters/line_trend_formatter.py
@@ -0,0 +1,12 @@
+
+class LineTrendFormatter(object):
+
+    def __init__(self, line_color='blue', line_width=2):
+        self._line_color = line_color
+        self._line_width = line_width
+
+    def format(self, x, y, plot):
+        """
+        Assumes plot is a matplotlib plot object.
+        """
+        plot.plot(x, y, color=self._line_color, linewidth=self._line_width)
---
+++ method/helpers/plotting/trend_formatters/no_trend_formatter.py
@@ -0,0 +1,8 @@
+
+class NoTrendFormatter(object):
+
+    def __init__(self):
+        pass
+
+    def format(self, *args):
+        pass
